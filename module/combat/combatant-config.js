71142fbd80edaa05b36e5fa12e94f30e3271bc8b
minciong <maxraphael_inciong@yahoo.com>
2022-02-14 19:32:52 -0500

    removed extraneous debug lines

--- module/combat/combatant-config.js
+++ module/combat/combatant-config.js
@@ -18,7 +18,7 @@ export class FFCombatantConfig extends DocumentSheet {
 
   /* -------------------------------------------- */
   getData(options){
-    console.log(super.getData(options))
+    
     return mergeObject(super.getData(options),{
       user:game.user
     });
@@ -28,12 +28,10 @@ export class FFCombatantConfig extends DocumentSheet {
     formData.initDice=formData.initDice.split(',').map(die=>parseInt(die)).sort(function(a, b){return a-b}).filter(d=>d>0&&d<=10)
     formData.flags={initDice:formData.initDice}
 
-    console.log(formData,typeof(formData));
+    
     if ( this.object.id ) {
-      formData.id = this.object.id;
       formData.initiative=(this.object.initiative*1000)%1000/1000+formData.initDice.reduce((a,b)=>a+b)
       delete formData.initDice;
-      console.log(formData);
       return this.object.update(formData);
     }
     else {
